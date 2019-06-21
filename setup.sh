b4a13634042d6b0caabc182c736594a0a529d07f
Peter Polovinkin <p.polovinkin@igniti.de>
2019-06-21 10:53:28 +0200

    trying out

--- setup.sh
+++ setup.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-#partitioning
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-  o # clear the in memory partition table
-  n # new partition
-  p # primary partition
-  1 # partition number 1
-    # default - start at beginning of disk 
-  +100M # 100 MB boot parttion
-  n # new partition
-  p # primary partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-  +16G # default, extend partition to end of disk
-  n
-  p
-  3
-	#
-  +16G
-  n
-  p
-  4
-	#
-    #   		
-  w # write the partition table
-  q # and we're done
-EOF
+#get total memory to calculate the SWAP size
+SWAP_SIZE=$(awk '/MemTotal/ { print int(($2/1000/1000)+0.5) }' /proc/meminfo)
+echo $SWAP_SIZE
+HOME_START=$(echo "25372672+$SWAP_SIZE" | bc)
 
+TOTAL_SECTORS=$(fdisk -l | awk 'NR==1{ print $7 }')
+HOME_SIZE=$(echo "$TOTAL_SECTORS - $HOME_START" | bc)
 
-
+echo $HOME_SIZE
 
 # install dependencies
 
