ad67fdd073395b0201ae08bcc16c3ea9ed773124
Peter Polovinkin <p.polovinkin@igniti.de>
2019-06-21 17:23:20 +0200

    feierabend

--- configuration.sh
+++ configuration.sh
@@ -14,8 +14,7 @@ exec 3>&1
 zone1=$(dialog --clear --title "ZONE" --no-tags --menu "Select your continent" 60 10 60 $(ls -l /usr/share/zoneinfo | grep '^d' | awk '{ print $9 }' | awk '{ print NR" "$0 }') 2>&1 1>&3 3>&-)
 continent=$(ls -l /usr/share/zoneinfo | grep '^d' | awk '{ print $9 }' | sed -n ${zone1}p)
 
-zone2=$(dialog --clear --title "ZONE" --no-tags --menu "Select your area" 60 10 60 $(ls -l /usr/share/zoneinfo/$continent | awk '{ print NR" "$0 }') 2>&1 1>&3 2>&3 3>&-)
-area=$(ls /usr/share/zoneinfo | grep '^d' | awk '{ print $9 }' | sed -n ${zone2}p)
+zone2=$(dialog --clear --title "ZONE" --no-tags --menu "Select your area" 60 10 60 $(ls /usr/share/zoneinfo/$continent | awk '{ print NR" "$0 }') 2>&1 1>&3 3>&-)
+area=$(ls /usr/share/zoneinfo/$continent | sed -n ${zone2}p)
 
 ln -sf /usr/share/zoneinfo/$continent/$area /etc/localtime
-ls -la
