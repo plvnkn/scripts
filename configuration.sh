3c28923baa4492e371fef33e8e299af1cc55f561
Peter Polovinkin <p.polovinkin@igniti.de>
2019-06-21 15:33:34 +0200

    testing

--- configuration.sh
+++ configuration.sh
@@ -9,6 +9,6 @@ echo LANG=$lang.UTF-8 > /etc/locale.conf
 keymap=$(dialog --clear --title "KEYMAP" --no-tags --menu "Select your keyboard mapping" 60 10 60 $(localectl list-keymaps) 3>&1 1>&2 2>&3 3>&-)
 echo KEYMAP=$keymap > /etc/vconsole.conf
 
-zone1=$(cd ..dialog --clear --title "ZONE" --no-tags --menu "Select your continent" 60 10 60 $(ls -l /usr/share/zoneinfo/ | grep '^d') 3>&1 1>&2 2>&3 3>&-)
-zone2=$(cd ..dialog --clear --title "ZONE" --no-tags --menu "Select your area" 60 10 60 $(ls -l /usr/share/zoneinfo/$zone1) 3>&1 1>&2 2>&3 3>&-)
+zone1=$(dialog --clear --title "ZONE" --no-tags --menu "Select your continent" 60 10 60 $(ls -l /usr/share/zoneinfo | grep '^d' | awk '{ print $9 }') 3>&1 1>&2 2>&3 3>&-)
+zone2=$(dialog --clear --title "ZONE" --no-tags --menu "Select your area" 60 10 60 $(ls -l /usr/share/zoneinfo/$zone1) 3>&1 1>&2 2>&3 3>&-)
 ln -sf /usr/share/zoneinfo/$zone1/$zone2 /etc/localtime
