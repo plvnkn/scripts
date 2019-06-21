26570124adaed345d3e057c0e8ee2d64a26a3086
Peter Polovinkin <p.polovinkin@igniti.de>
2019-06-21 11:12:24 +0200

    added partition layout config file

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 #get total memory to calculate the SWAP size
-SWAP_SIZE=$(awk '/MemTotal/ { print int(($2/1000/1000)+0.5) }' /proc/meminfo)
-echo $SWAP_SIZE
-HOME_START=$(echo "25372672+$SWAP_SIZE" | bc)
-
+SWAP_SIZE_GB=$(awk '/MemTotal/ { print int(($2/1000/1000)+0.5) }' /proc/meminfo)
+SWAP_SIZE=$(echo "$SWAP_SIZE_GB * 1024*1024 *2" | bc)
+HOME_START=$(echo "25372672+($SWAP_SIZE_GB * 1024*1024 *2)" | bc)
 TOTAL_SECTORS=$(fdisk -l | awk 'NR==1{ print $7 }')
 HOME_SIZE=$(echo "$TOTAL_SECTORS - $HOME_START" | bc)
 
-echo $HOME_SIZE
+
+#replace placeholders in partition layout template
+sed -e "s/\${SWAP_SIZE}/$SWAP_SIZE/" \
+	-e "s/\${HOME_START}/$HOME_START/" \
+	-e "s/\${HOME_SIZE}/$HOME_SIZE/" \
+	config/partition-layout-template > partition-config
 
 # install dependencies
 
