67f1fde87d879364a542ed1e3715f23451490b46
Peter Polovinkin <p.polovinkin@igniti.de>
2019-06-21 15:13:15 +0200

    seperated config and setup

---
+++ configuration.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#confiuration
+read -p 'Hostname: ' HOST_NAME
+echo $HOST_NAME > /etc/hostname
+
+lang=$(dialog --clear --title "LANG" --no-tags --menu "Select your locale" 60 10 60 $(ls /usr/share/i18n/locales) 3>&1 1>&2 2>&3 3>&-)
+echo LANG=$lang.UTF-8 > /etc/locale.conf 
+
+keymap=$(dialog --clear --title "KEYMAP" --no-tags --menu "Select your keyboard mapping" 60 10 60 $(localectl list-keymaps) 3>&1 1>&2 2>&3 3>&-)
+echo KEYMAP=$keymap > /etc/vconsole.conf
+
+zone1=$(cd ..dialog --clear --title "ZONE" --no-tags --menu "Select your continent" 60 10 60 $(ls -l /usr/share/zoneinfo/ | grep '^d') 3>&1 1>&2 2>&3 3>&-)
+zone2=$(cd ..dialog --clear --title "ZONE" --no-tags --menu "Select your area" 60 10 60 $(ls -l /usr/share/zoneinfo/$zone1) 3>&1 1>&2 2>&3 3>&-)
+ln -sf /usr/share/zoneinfo/$zone1/$zone2 /etc/localtime
--- setup.sh
+++ setup.sh
@@ -35,24 +35,9 @@ mkdir -p /mnt/boot
 mount /dev/sda4 /mnt/home
 mount /dev/sda1	/mnt/boot
 
+wget https://raw.githubusercontent.com/plvnkn/scripts/master/configuration.sh -O /mnt/home/configuration.sh
+
 #install arch
 pacstrap /mnt base base-devel wpa_supplicant dialog bash-completion
-echo "genfstab -Up /mnt > /mnt/etc/fstab"
 genfstab -Up /mnt > /mnt/etc/fstab
-
-echo "arch-chroot /mnt";
 arch-chroot /mnt
-
-#confiuration
-read -p 'Hostname: ' HOST_NAME
-echo $HOST_NAME > /etc/hostname
-
-lang=$(dialog --clear --title "LANG" --no-tags --menu "Select your locale" 60 10 60 $(ls /usr/share/i18n/locales) 3>&1 1>&2 2>&3 3>&-)
-echo LANG=$lang.UTF-8 > /etc/locale.conf 
-
-keymap=$(dialog --clear --title "KEYMAP" --no-tags --menu "Select your keyboard mapping" 60 10 60 $(localectl list-keymaps) 3>&1 1>&2 2>&3 3>&-)
-echo KEYMAP=$keymap > /etc/vconsole.conf
-
-zone1=$(cd ..dialog --clear --title "ZONE" --no-tags --menu "Select your continent" 60 10 60 $(ls -l /usr/share/zoneinfo/ | grep '^d') 3>&1 1>&2 2>&3 3>&-)
-zone2=$(cd ..dialog --clear --title "ZONE" --no-tags --menu "Select your area" 60 10 60 $(ls -l /usr/share/zoneinfo/$zone1) 3>&1 1>&2 2>&3 3>&-)
-ln -sf /usr/share/zoneinfo/$zone1/$zone2 /etc/localtime
