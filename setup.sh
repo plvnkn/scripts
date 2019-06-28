a7bd7bd1880b8d2d42b1247061382774c9d69927
Peter Polovinkin <p.polovinkin@igniti.de>
2019-06-28 10:00:50 +0200

    fixing

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ boot="+200M"
 root=$(inputBox "Root partition in GB" "Partitioning" "Root")
 swap=$(awk '/MemTotal/ { print int(($2/1000/1000)+0.5) }' /proc/meminfo)
 
+
+
 cat <<EOF | fdisk /dev/sda
 o
 n
@@ -30,8 +32,6 @@ p
 
 w
 EOF
-
-
 
 mkfs.ext4 -L BOOT /dev/sda1
 mkfs.ext4 -L ROOT /dev/sda2
