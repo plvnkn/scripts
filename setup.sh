3a5fe67b20b954082eeba493a6c954c3913781b9
Peter Polovinkin <p.polovinkin@igniti.de>
2019-07-03 09:26:13 +0200

    removed duplicate lvm create

--- setup.sh
+++ setup.sh
@@ -17,25 +17,18 @@ cat <<EOF cryptsetup luksOpen /dev/sda1 disk
 passwd
 EOF
 
-pvcreate /dev/mapper/root
-vgcreate vol /dev/mapper/root
-
-
 #get total memory to calculate the SWAP size
 
 dialog --no-cancel --inputbox "Root partition size in GB" 10 60 2> root
-g="G"
-root="$root$g"
 
 swap=$(awk '/MemTotal/ { print int(($2/1000/1000)+0.5) }' /proc/meminfo)
-swap="$swap$g"
 
 #lvm
-pvcreate /dev/sda
-vgcreate main /dev/sda
-lvcreate -L ${root}G root -n /dev/sda
-lvcreate -L ${swap}G swap -n /dev/sda
-lvcreate -l 100%FREE  home -n /dev/sda
+pvcreate /dev/sda1
+vgcreate main /dev/sda1
+lvcreate -L ${root}G main -n root
+lvcreate -L ${swap}G main -n swap
+lvcreate -l 100%FREE main -n home
 
 mkfs.ext4 -L ROOT /dev/main/root
 mkfs.ext4 -L HOOT /dev/main/home
