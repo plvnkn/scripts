16d404b179918d325293472497a6be20a06ec48b
Peter Polovinkin <peter.polovinkin@gmail.com>
2019-07-19 22:17:01 +0200

    changed back to passwd

--- usermanagement/setPasswd.sh
+++ usermanagement/setPasswd.sh
@@ -6,7 +6,4 @@ if [ -z $1 ]; then pwForUser="root"; else pwForUser="$1"; fi
 
 password_dialog "Password ($pwForUser)" "User"
 
-cat <<EOF | passwd "$pwForUser"
-$passwd_User
-$passwd_User
-EOF
+echo -e $(esc "$passwd_User")\"\\n\"$(esc "$passwd_User") | passwd "$pwForUser"
