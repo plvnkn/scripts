79cf30d7513cca296657362b7461520eca0d1cdc
Peter Polovinkin <p.polovinkin@igniti.de>
2019-07-19 16:01:33 +0200

    fixing password dialog

--- usermanagement/setPasswd.sh
+++ usermanagement/setPasswd.sh
@@ -6,4 +6,7 @@ if [ -z $1 ]; then pwForUser="root"; else pwForUser="$1"; fi
 
 password_dialog "Password ($pwForUser)" "User"
 
-echo ${passwd_User} | passwd --stdin $pwForUser
+cat <<EOF | passwd "pwForUser"
+${passwd_User}
+${passwd_User}
+EOF
