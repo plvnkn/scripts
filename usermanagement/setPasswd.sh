902569b385510ccb9949883723a18430272ed9a1
Peter Polovinkin <peter.polovinkin@gmail.com>
2019-07-19 21:14:49 +0200

    trying smth out

--- usermanagement/setPasswd.sh
+++ usermanagement/setPasswd.sh
@@ -7,6 +7,6 @@ if [ -z $1 ]; then pwForUser="root"; else pwForUser="$1"; fi
 password_dialog "Password ($pwForUser)" "User"
 
 cat <<EOF | passwd "$pwForUser"
-${passwd_User}
-${passwd_User}
+$passwd_User
+$passwd_User
 EOF
