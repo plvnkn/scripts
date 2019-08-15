0916df87da4651a839385311e0ed54d8d9eacbd4
Peter Polovinkin <p.polovinkin@igniti.de>
2019-08-15 10:38:31 +0200

    bugfixing

--- setup-software.sh
+++ setup-software.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+sudo pacman -S git
+
 #first install yay to be able to install aur packages#
-cd ~/tools
+cd tools
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-cd ~
+cd -
 
 while IFS=, read -r package type
 do
 	case "$type" in
 			"") 
-				pacman --noconfirm --needed -Syy "$package" 
+				sudo pacman --noconfirm --needed -Syy "$package" 
 				;;
 			"a") 
 				yay --noconfirm -S "$program" 
